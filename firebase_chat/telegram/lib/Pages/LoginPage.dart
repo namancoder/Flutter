dca9f894971489e9adb53251e3eac96d702b9aae
namancoder <namanitsofficial@gmail.com>
2021-05-16 05:14:04 +0530

    Chat app

---
+++ firebase_chat/telegram/lib/Pages/LoginPage.dart
@@ -0,0 +1,174 @@
+import 'dart:async';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:telegramchatapp/Pages/HomePage.dart';
+import 'package:telegramchatapp/Widgets/ProgressWidget.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class LoginScreen extends StatefulWidget {
+  LoginScreen({Key key}) : super(key: key);
+  @override
+  LoginScreenState createState() => LoginScreenState();
+}
+
+class LoginScreenState extends State<LoginScreen> {
+  final GoogleSignIn googleSignIn = GoogleSignIn();
+  final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+  SharedPreferences preferences;
+
+  bool isLoggedIn = false;
+  bool isLoading = false;
+
+  FirebaseUser currentUser;
+
+  @override
+  void initState() {
+    super.initState();
+    isSignedIn();
+  }
+
+  isSignedIn() async {
+    this.setState(() {
+      isLoggedIn = true;
+    });
+
+    preferences = await SharedPreferences.getInstance();
+    isLoggedIn = await googleSignIn.isSignedIn();
+
+    if (isLoggedIn) {
+      Navigator.push(
+          context,
+          MaterialPageRoute(
+              builder: (context) =>
+                  HomeScreen(currentUserId: preferences.getString("id"))));
+    }
+    this.setState(() {
+      isLoading = false;
+    });
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topRight,
+            end: Alignment.bottomLeft,
+            colors: [Colors.black, Colors.deepPurple[900]],
+          ),
+        ),
+        alignment: Alignment.bottomCenter,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "Chat App",
+              style: TextStyle(
+                  fontSize: 82.0, color: Colors.white, fontFamily: "Signatra"),
+            ),
+            GestureDetector(
+              onTap: controlSignIn,
+              child: Center(
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      width: 270.0,
+                      height: 65.0,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage(
+                              "assets/images/google_signin_button.png"),
+                          fit: BoxFit.fill,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(1.0),
+                      child: isLoading ? circularProgress() : Container(),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Future<Null> controlSignIn() async {
+    preferences = await SharedPreferences.getInstance();
+    this.setState(() {
+      isLoading = true;
+    });
+    GoogleSignInAccount googleUser = await googleSignIn.signIn();
+    GoogleSignInAuthentication googleAuthentication =
+        await googleUser.authentication;
+
+    final AuthCredential credential = GoogleAuthProvider.getCredential(
+        idToken: googleAuthentication.idToken,
+        accessToken: googleAuthentication.accessToken);
+
+    FirebaseUser firebaseUser =
+        (await firebaseAuth.signInWithCredential(credential)).user;
+        
+    if (firebaseUser != null) {
+      //check if already signed up;
+String Name = firebaseUser.displayName;
+      final QuerySnapshot resultQuery = await Firestore.instance
+          .collection("users")
+          .where("id", isEqualTo: firebaseUser.uid)
+          .getDocuments();
+
+      final List<DocumentSnapshot> documentSnapshots = resultQuery.documents;
+
+      if (documentSnapshots.length == 0) {
+        Firestore.instance
+            .collection("users")
+            .document(firebaseUser.uid)
+            .setData({
+          "nickname": firebaseUser.displayName,
+          "photoUrl": firebaseUser.photoUrl,
+          "id": firebaseUser.uid,
+          "aboutMe": "get a life",
+          "createdAt": DateTime.now().millisecondsSinceEpoch.toString(),
+          "chattingWith": null,
+        });
+
+        //write data in local
+        currentUser = firebaseUser;
+        await preferences.setString("id", currentUser.uid);
+        await preferences.setString("nickname", currentUser.displayName);
+        await preferences.setString("photoUrl", currentUser.photoUrl);
+      } else {
+        await preferences.setString("id", documentSnapshots[0]["id"]);
+        await preferences.setString(
+            "nickname", documentSnapshots[0]["nickname"]);
+        await preferences.setString(
+            "photoUrl", documentSnapshots[0]["photoUrl"]);
+        await preferences.setString(
+            "photoUrl", documentSnapshots[0]["aboutMe"]);
+      }
+
+      Fluttertoast.showToast(msg: "Welcome $Name");
+      this.setState(() {
+        isLoading = false;
+      });
+      Navigator.push(
+          context,
+          MaterialPageRoute(
+              builder: (context) =>
+                  HomeScreen(currentUserId: firebaseUser.uid)));
+    } else {
+      Fluttertoast.showToast(msg: "Try Again");
+      this.setState(() {
+        isLoading = false;
+      });
+    }
+  }
+}
